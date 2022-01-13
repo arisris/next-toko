aa180b237d65713e6f1a4af526c1dbc70a8e8adb
arisris <aris@mail.kliksob.com>
2022-01-13 08:46:03 +0700

    Add custom screen size

---
+++ libs/screen-size.js
@@ -0,0 +1,15 @@
+// @ts-ignore
+/** @type {{xxxxs: number, xxxs: number, xxs: number, xs: number, sm: number, md: number, lg: number, xlg: number, xl: number, 2xl: number, max: number}} */
+module.exports = {
+  xxxxs: 260,
+  xxxs: 319,
+  xxs: 359,
+  xs: 410,
+  sm: 640,
+  md: 768,
+  lg: 1024,
+  xlg: 1150,
+  xl: 1280,
+  "2xl": 1536,
+  max: 1920
+};
