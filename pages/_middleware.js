4df87b82006de1906bde13f3519886fd7c4ada3f
arisris <aris@mail.kliksob.com>
2022-01-14 07:24:38 +0700

    todo middleware

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -1,12 +1,27 @@
-import { NextResponse } from 'next/server';
+// import { getToken } from "next-auth/jwt";
+import { NextResponse } from "next/server";
 
 /**
+ * Note: This middleware run only on Node server or Edge Runtime like vercel
  * @type {import("next/server").NextMiddleware}
  */
 export default async function _middleware(req, event) {
   const response = NextResponse.next();
-  // set app name
-  response.headers.set('X-Site-Name', 'Arisris.con');
+  
+  /**
+   * Todo the next-auth session can be reading with this
+   */
 
+  // const user = await getToken({
+  //   req,
+  //   secret: process.env.APP_SECRET_KEY,
+  //   secureCookie:
+  //     process.env.NEXTAUTH_URL?.startsWith("https://") ??
+  //     !!process.env.VERCEL_URL
+  // });
+  // console.log(user);
+
+  // this is example to append headers object at all request
+  response.headers.set("X-Site-Name", "Arisris.con");
   return response;
 }
