12fe24c9f77de7fd9e8a71a0c8a3329ca5f32620
arisris <aris@mail.kliksob.com>
2022-02-10 20:13:01 +0700

    Some Changes

--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -1,5 +1,6 @@
 import AdminPageLayout from "@/components/Layouts/AdminPage";
-import { useToast } from "@/lib/hooks/useToast";
+import { useHeadlessuiDialog } from "@/lib/hooks/useHeadlessuiDialog";
+import { ToastType, useToast } from "@/lib/hooks/useToast";
 import { Portal } from "@headlessui/react";
 import { Button, Toast } from "konsta/react";
 import { GetServerSideProps, NextPageContext } from "next";
@@ -8,6 +9,7 @@ import { Fragment } from "react";
 
 export default function AdminPageIndex() {
   const toast = useToast();
+  const dialog = useHeadlessuiDialog();
 
   return (
     <AdminPageLayout>
@@ -15,11 +17,19 @@ export default function AdminPageIndex() {
         <h3>This is Admin Page</h3>
         <h3>This is Admin Page</h3>
       </div>
-      <Button inline onClick={() => {
-        toast.message({
-          title: "Hello World"
-        })
-      }}>Toast</Button>
+      <Button
+        inline
+        onClick={() => {
+          // toast.message({
+          //   title: "Hello World",
+          //   message: "Whoaaaaa!!!",
+          //   type: ToastType.SUCCESS
+          // })
+          dialog.create(<h2>Hello World</h2>);
+        }}
+      >
+        Toast
+      </Button>
     </AdminPageLayout>
   );
 }
